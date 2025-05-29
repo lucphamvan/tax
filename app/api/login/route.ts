41802c46f137bbf5ec426fccb2ede2687f7e6ddf
jugmirana <jug.mirana@gmail.com>
2025-05-30 00:04:09 +0700

    debg

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -4,7 +4,9 @@ import { cookies } from 'next/headers'
 
 export async function POST(request: Request) {
     try {
+        console.log('Login API called')
         const data = await request.json()
+        console.log('Request data:', data)
         const response = await axios.post('https://hoadondientu.gdt.gov.vn:30000/security-taxpayer/authenticate', data, {
             httpsAgent: agent,
         })
@@ -14,6 +16,7 @@ export async function POST(request: Request) {
         })
         return Response.json({ token }, { status: 200 })
     } catch (error: any) {
+        console.error('Login API error:', error.message)
         return Response.json(error.response.data, { status: 400 })
     }
 }
